cd68903d1fc5b427565afb6897f4d02ad86c2509
manrick <manricksantos@gmail.com>
2023-06-22 15:09:21 -0300

    consumindo api

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,7 +1,54 @@
 import styles from './table.module.css';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+interface CoinProps {
+  symbol: string, 
+  show_symbol: boolean,
+  delta_24: string,
+  volume_24: string,
+  market_cap: string,
+  name: string,
+  rank: number,
+  price: string, 
+  formatedPrice: string,
+  formatedMarket: string
+}
+
+interface DataProps {
+  coins: CoinProps[]
+}
 
 export function Table() {
+  const [coins, setCoins] = useState<CoinProps[]>([]);
+
+  useEffect(() => {
+    function getData(){
+      fetch('https://sujeitoprogramador.com/api-cripto/?key=0125db9f210b2f19&pref=BRL')
+      .then(response => response.json())
+      .then((data: DataProps) => {
+        const coinsList = data.coins.slice(0, 20);
+
+        const price = Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        })
+
+        const formatedResult = coinsList.map((data) => {
+          const formated = {
+            ...data,
+            formatedPrice: price.format(Number(data.price)),
+            formatedMarket: price.format(Number(data.market_cap))
+          }
+          return formated
+        })
+        
+        setCoins(formatedResult)
+      })
+    }
+    getData();
+  }, [])
+  
   return(
     <div className={styles.container}>
       <table className={styles.table}>
